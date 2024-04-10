02b937f0c765fd07fc500fc246702597ee325af5
2un-light <sts07129@naver.com>
2024-04-11 01:22:49 +0900

    [Setting] 개발환경 세팅

---
+++ src/test/java/com/damayoproject/damayo/DamayoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damayoproject.damayo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamayoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
