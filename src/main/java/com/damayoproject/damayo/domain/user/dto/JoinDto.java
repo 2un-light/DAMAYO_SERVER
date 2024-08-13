cac06af9059a89c357e0efee257b8ff98738264c
2un-light <sts07129@naver.com>
2024-08-13 17:58:42 +0900

    [Feature] validation 로직 추가

--- src/main/java/com/damayoproject/damayo/domain/user/dto/JoinDto.java
+++ src/main/java/com/damayoproject/damayo/domain/user/dto/JoinDto.java
@@ -1,5 +1,9 @@
 package com.damayoproject.damayo.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,8 +13,18 @@ import lombok.ToString;
 @Setter
 @ToString
 @NoArgsConstructor
+
 public class JoinDto {
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 양식을 확인해주세요")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "비밀번호는 하나 이상의 영문자, 숫자 및 특수문자를 포함해야 합니다.")
     private String password;
 }
