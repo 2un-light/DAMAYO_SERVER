3d27dd9b5352463ae1c1550903a0b9041b39e841
2un-light <sts07129@naver.com>
2024-08-13 17:58:45 +0900

    [Feature] validation 로직 추가

--- src/main/java/com/damayoproject/damayo/domain/user/dto/SignInDto.java
+++ src/main/java/com/damayoproject/damayo/domain/user/dto/SignInDto.java
@@ -1,5 +1,7 @@
 package com.damayoproject.damayo.domain.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -7,6 +9,11 @@ import lombok.*;
 @ToString
 @NoArgsConstructor
 public class SignInDto {
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "이메일 양식을 확인해주세요")
     private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 }
