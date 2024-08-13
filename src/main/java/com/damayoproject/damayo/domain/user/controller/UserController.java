fd9c53b4ab618eedfaec2c2eac7a67b80ee07345
차은영 <sts07129@naver.com>
2024-08-13 17:51:37 +0900

    [API] ApiResponse 응답양식 통일 (#9)
    * [Api] API 공통 응답 적용
    
    * [Api] API 공통 응답 적용

--- src/main/java/com/damayoproject/damayo/domain/user/controller/UserController.java
+++ src/main/java/com/damayoproject/damayo/domain/user/controller/UserController.java
@@ -4,11 +4,16 @@ import com.damayoproject.damayo.domain.user.dto.JoinDto;
 import com.damayoproject.damayo.domain.user.dto.SignInDto;
 import com.damayoproject.damayo.domain.user.service.UserService;
 import com.damayoproject.damayo.global.config.security.JwtToken;
-import com.damayoproject.damayo.global.response.ResponseDto;
-import com.damayoproject.damayo.global.response.ResponseUtil;
+import com.damayoproject.damayo.global.response.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -19,18 +24,33 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/join")
-    public ResponseDto join(@RequestBody JoinDto joinDto){
+    public ResponseEntity<ApiResponse<String>> join(@Valid @RequestBody JoinDto joinDto, BindingResult result){
+
+        if(result.hasErrors()) {
+            String errorMsg = result.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+            ApiResponse<String> apiResponse = new ApiResponse<>(HttpStatus.BAD_REQUEST.value(), errorMsg, null);
+            return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
+        }
+
         userService.join(joinDto);
-        return ResponseUtil.SUCCESS("회원가입에 성공하였습니다.", null);
+
+        String responseMessage = "회원가입이 성공적으로 완료되었습니다.";
+        ApiResponse<String> apiResponse = new ApiResponse<>(HttpStatus.OK.value(), responseMessage, null);
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 
     @PostMapping("/sign-in")
-    public ResponseDto signIn(@RequestBody SignInDto signInDto){
+    public ResponseEntity<ApiResponse<JwtToken>> signIn(@Valid @RequestBody SignInDto signInDto){
         String email = signInDto.getEmail();
         String password = signInDto.getPassword();
         JwtToken jwtToken = userService.signIn(email, password);
-        return ResponseUtil.SUCCESS("로그인 성공", jwtToken);
-}
+
+        String responseMessage = "로그인이 성공적으로 완료되었습니다.";
+        ApiResponse<JwtToken> apiResponse = new ApiResponse<>(HttpStatus.OK.value(), responseMessage, jwtToken);
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
 
     @PostMapping("/test")
     public String test() {
