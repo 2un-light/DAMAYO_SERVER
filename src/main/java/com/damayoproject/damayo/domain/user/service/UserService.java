fc0ab7869b34d94fc1fefc0f280e7b7414d7d680
2un-light <sts07129@naver.com>
2024-08-22 16:12:50 +0900

    [Feature] 예외처리 로직 추가

--- src/main/java/com/damayoproject/damayo/domain/user/service/UserService.java
+++ src/main/java/com/damayoproject/damayo/domain/user/service/UserService.java
@@ -5,6 +5,9 @@ import com.damayoproject.damayo.domain.user.entity.User;
 import com.damayoproject.damayo.domain.user.repository.UserRepository;
 import com.damayoproject.damayo.global.config.security.JwtToken;
 import com.damayoproject.damayo.global.config.security.JwtTokenProvider;
+import com.damayoproject.damayo.domain.user.exception.EmailAlreadyExistsException;
+import com.damayoproject.damayo.domain.user.exception.EmailNotFoundException;
+import com.damayoproject.damayo.domain.user.exception.PasswordMismatchException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -32,11 +35,23 @@ public class UserService {
                 .password(passwordEncoder.encode(joinDto.getPassword()))
                 .build();
 
+        if(userRepository.existsByEmail(user.getEmail())){
+            throw new EmailAlreadyExistsException("중복되는 이메일이 존재합니다.");
+        }
+
         return userRepository.save(user).getId();
     }
 
     @Transactional
     public JwtToken signIn(String email, String password) {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new EmailNotFoundException("이메일을 다시 확인해주세요."));
+
+        if(!passwordEncoder.matches(password, user.getPassword())){
+            throw new PasswordMismatchException("비밀번호가 일치하지 않습니다.");
+        }
+
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password);
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
         JwtToken jwtToken = jwtTokenProvider.accessToken(authentication);
